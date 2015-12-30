5926a05641c0cd0d573e73b5f2ef285fb24e8557
Marcin Kumorek <kumeek@gmail.com>
2015-12-30 21:43:44 +0000

    Add material-ui, first test component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import MessageList from './MessageList.jsx'
+
+import { AppBar } from 'material-ui'
+import ThemeManager from 'material-ui/lib/styles/theme-manager'
+import BaseTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme'
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin()
 
 export default class App extends Component {
 
@@ -9,7 +20,20 @@ export default class App extends Component {
 		super(props)
 	}
 
+	static childContextTypes = {
+		muiTheme: PropTypes.object
+	}
+
+	getChildContext() {
+		return { muiTheme: ThemeManager.getMuiTheme(BaseTheme) }
+	}
+
 	render() {
-		return <MessageList />
+		return (
+			<div>
+				<AppBar title="My first material-ui component!" />
+				<MessageList />
+			</div>
+		)
 	}
 }
